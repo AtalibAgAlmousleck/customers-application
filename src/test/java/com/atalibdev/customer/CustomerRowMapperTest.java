952caf4376392f82594b63426bbb0d1afcec5ccb
AtalibAgAlmousleck <atalibletouareg@gmail.com>
2023-10-13 17:34:04 +0800

    Write the IntegrationTest for CustomerController

--- src/test/java/com/atalibdev/customer/CustomerRowMapperTest.java
+++ src/test/java/com/atalibdev/customer/CustomerRowMapperTest.java
@@ -1,23 +1,32 @@
 package com.atalibdev.customer;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CustomerRowMapperTest {
 
-    @BeforeEach
-    void setUp() {
-    }
-
     @Test
-    void mapRow() {
+    void mapRow() throws SQLException {
         // Given
-
+        CustomerRowMapper customerRowMapper = new CustomerRowMapper();
+        ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.when(resultSet.getLong("id")).thenReturn(1L);
+        Mockito.when(resultSet.getInt("age")).thenReturn(20);
+        Mockito.when(resultSet.getString("name")).thenReturn("Java");
+        Mockito.when(resultSet.getString("email")).thenReturn("java@gmail.com");
 
         // When
+        Customer actual = customerRowMapper.mapRow(resultSet, 1);
 
         // Then
+        Customer exceptedCustomer = new Customer(
+                1L, "Java", "java@gmail.com", 20
+        );
+        assertThat(actual).isEqualTo(exceptedCustomer);
     }
 }
---
+++ src/test/java/com/atalibdev/testController/CustomerIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.atalibdev.testController;
+
+import com.atalibdev.customer.CustomerRegistrationRequest;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Random;
+import java.util.UUID;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class CustomerIntegrationTest {
+
+    private static final Random RANDOM = new Random();
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void canRegisterCustomer() {
+        // create registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + UUID.randomUUID() + "@google.com";
+        int age = RANDOM.nextInt(18, 99);
+        CustomerRegistrationRequest request =
+                new CustomerRegistrationRequest(name, email, age);
+        // send post request
+        webTestClient.post()
+                .uri("/api/v1/customers")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerRegistrationRequest.class)
+                .exchange()
+                .expectStatus().isOk();
+
+        // get customers
+
+        // make sure that the customer is present
+
+        // get customer by id
+    }
+}
